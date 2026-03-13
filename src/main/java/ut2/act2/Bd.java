793b6138ff90e2176b3157b6b03abdef6d1053c5
Pau Aldea <paualdeabatista@protonmail.com>
2026-03-13 23:07:56 +0100

    Creación estructura básica menu + Bd

--- src/main/java/ut2/act2/Bd.java
+++ src/main/java/ut2/act2/Bd.java
@@ -1,8 +1,36 @@
 package ut2.act2;
 
+// IMPORTS
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Bd {
     // Dirección BD SQLite
     final String url = "jdbc:sqlite:tiendaDB.db";
 
+    /**
+     * Función constructora de la clase, que sirve para crear o identificar las tablas de la BD
+     */
+    public Bd () {
+        // Creamos la sentencia para crear la TABLA productos en la BD
+        String productos = "CREATE TABLE IF NOT EXISTS PRODUCTOS(id_producto INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100), stock INTEGER);";
+        String ventas = "CREATE TABLE IF NOT EXISTS VENTAS(id_venta INTEGER PRIMARY KEY AUTOINCREMENT, unidades INTEGER, fecha DATE, FOREIGN KEY(id_producto) REFERENCES PRODUCTOS(id_producto));";
 
+        // Implementamos un try-with-resources para no desperdiciar recursos
+        try (Connection c = DriverManager.getConnection(url);
+             PreparedStatement ps = c.prepareStatement(productos);
+             PreparedStatement ps2 = c.prepareStatement(ventas))
+        {
+            // Ejecutamos la creación de las tablas siguiendo las sentencias
+            ps.executeUpdate();
+            ps2.executeUpdate();
+        }
+        // En caso de que falle la conexión a la BD, controlamos la excepción (SQLException)
+        catch (SQLException e) {
+            // Mandamos el error usando System.err para no ensuciar la salida
+            System.err.println("Error SQL: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/ut2/act2/Main.java
+++ src/main/java/ut2/act2/Main.java
@@ -1,7 +1,96 @@
 package ut2.act2;
 
-public class Main {
-    public static void main(String[] args) {
+// IMPORTS
+import java.util.Scanner;
 
+public class Main {
+    // Constante para el tiempo de espera
+    final static int TIEMPO_ESPERA = 1250;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+        int opcion = 0;
+
+        // Creamos la BD creando una instancia de la clase Bd.java
+        Bd bd = new Bd();
+
+        // Mientras que salir sea false, seguir el bucle while
+        while (!salir) {
+            limpiarPantalla();
+            System.out.print("\t\t.:TIENDA DB:.\n\n1. Crear venta\n2. Añadir stock\n3. Resetear BD\n4. Salir\nOpción: ");
+
+            try {
+                opcion = sc.nextInt();
+            } catch (Exception e) {
+                // Saltamos de línea para que no de errores de puntero
+                sc.nextLine();
+
+                System.err.println("Opción incorrecta\n");
+                espera(0);
+            }
+
+            switch (opcion) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    // Ponemos salir en true para romper el bucle while
+                    salir = true;
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Esta función limpia la pantalla dependiendo del sistema operativo que tengas
+     */
+    public static void limpiarPantalla() {
+        try {
+            // Obtenemos el sistema operativo desde el que se ejecuta el programa
+            String so = System.getProperty("os.name").toLowerCase();
+
+            // Si es windows lanzamos el comando cls para borrar la pantalla
+            if (so.contains("win")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+            // Si es Linux o Mac, lanzamos una secuencia de caracteres ANSI que limpia y borra la pantalla
+            else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            System.err.println("Error al limpiar la pantalla.\n" + e.getMessage());
+        }
+    }
+
+    /**
+     * Esta función ejecuta un bloque de código que para la ejecución de espera TIEMPO_ESPERA segundos
+     */
+    public static void espera(int tiempo) {
+        if (tiempo > 0) {
+            try {
+                Thread.sleep(tiempo);
+
+            } catch (InterruptedException e) {
+                System.err.println("No se ha podido hacer la pausa de " + tiempo);
+            }
+        } else {
+            try {
+                Thread.sleep(TIEMPO_ESPERA);
+
+            } catch (InterruptedException e) {
+                System.err.println("No se ha podido hacer la pausa de " + TIEMPO_ESPERA);
+            }
+        }
     }
 }
